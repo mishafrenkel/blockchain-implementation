cce48cfac0cd1e5d422548c9d793aa2d6b00635b
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-09 11:43:36 -0700

    test Proof of Work algorithm - consensus working

--- dev/test.js
+++ dev/test.js
@@ -9,6 +9,7 @@ const bitcoin = new Blockchain();
 // bitcoin.createNewBlock(2389, 'OIUOERDEGWKHKD', '78s97d4x6dsf');
 
 // Testing creatingNewBlock, then adding pending transaction, then creating newblock which adds the transaction
+
 // bitcoin.createNewBlock(789457, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56');
 // bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
 // bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WPLS214R7T6SJ3G2');
@@ -29,14 +30,27 @@ const bitcoin = new Blockchain();
 // console.log(bitcoin);
 
 
+// Testing PROOF OF WORK Logic
 const previousBlockHash = '87765DA6CCF0668238C1D27C35692E11';
 const currentBlockData = [
   {
-    amount: 10,
-    sender: 'B4CEE9C0E5CD571',
-    recipient: '3A3F6E462D48E9',
+    amount: 50,
+    sender: 'ALEXHT845SJ5TKCJ2',
+    recipient: 'JENN5BG5DF6HT8NG9'
+  },
+  {
+    amount: 200,
+    sender: 'ALEXHT845SJ5TKCJ2',
+    recipient: 'JENN5BG5DF6HT8NG9'
+  },
+  {
+    amount: 300,
+    sender: 'ALEXHT845SJ5TKCJ2',
+    recipient: 'JENN5BG5DF6HT8NG9'
   }
 ];
-const nonce = 100;
+// const nonce = 100;
 
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+// console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 7049));
