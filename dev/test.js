b35354d737ee9954313e4d29cae49a9f9e4f78b2
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-08 20:32:41 -0700

    unit test passing

--- dev/test.js
+++ dev/test.js
@@ -9,4 +9,5 @@ const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(789457, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56');
 bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
-console.log(bitcoin)
+bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WPLS214R7T6SJ3G2');
+console.log(bitcoin.chain[1])
