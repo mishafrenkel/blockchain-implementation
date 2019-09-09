4ed037c2c3d2a5c468d5c36dbce3d9dc81451623
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-08 19:45:21 -0700

    implement createNewBlock on Blockchain prototype

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -5,6 +5,16 @@ function Blockchain() {
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
   const newBlock = {
-
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.newTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash
   };
+
+  this.newTransaction = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
 };
