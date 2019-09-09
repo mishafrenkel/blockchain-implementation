1911c7b092dc5a9206e3d59bbf7f3bb83a8e2a30
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-08 19:56:20 -0700

    unit test Blockchain constructor

---
+++ dev/test.js
@@ -0,0 +1,10 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
+bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
+bitcoin.createNewBlock(2389, 'OKJOERETHKHKD', '78s97d4x6dsf');
+bitcoin.createNewBlock(2389, 'OIUOERDEGWKHKD', '78s97d4x6dsf');
+
+console.log(bitcoin);
