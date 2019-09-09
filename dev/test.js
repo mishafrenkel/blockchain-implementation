d13197b8431a2a06368eff213731e1c422d2600b
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-08 20:29:33 -0700

    add unit test for createNewTransaction

--- dev/test.js
+++ dev/test.js
@@ -2,9 +2,11 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
-bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
-bitcoin.createNewBlock(2389, 'OKJOERETHKHKD', '78s97d4x6dsf');
-bitcoin.createNewBlock(2389, 'OIUOERDEGWKHKD', '78s97d4x6dsf');
+// bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
+// bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
+// bitcoin.createNewBlock(2389, 'OKJOERETHKHKD', '78s97d4x6dsf');
+// bitcoin.createNewBlock(2389, 'OIUOERDEGWKHKD', '78s97d4x6dsf');
 
-console.log(bitcoin);
+bitcoin.createNewBlock(789457, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56');
+bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+console.log(bitcoin)
