77cbba9cb5500130f4f8ecbea853a3573d6f9b0b
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-08 20:47:25 -0700

    implement unit test for hashing function

--- dev/test.js
+++ dev/test.js
@@ -15,15 +15,28 @@ const bitcoin = new Blockchain();
 // console.log(bitcoin.chain[1])
 
 
-bitcoin.createNewBlock(789457, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56');
+// bitcoin.createNewBlock(789457, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56');
 
-bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+// bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
 
-bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WPLS214R7T6SJ3G2');
+// bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WPLS214R7T6SJ3G2');
 
-bitcoin.createNewTransaction(50, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
-bitcoin.createNewTransaction(200, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
-bitcoin.createNewTransaction(300, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
-bitcoin.createNewBlock(998764, 'AKMC875ZYS1TD9', 'WPLS214R7T6SJ292');
+// bitcoin.createNewTransaction(50, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+// bitcoin.createNewTransaction(200, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+// bitcoin.createNewTransaction(300, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+// bitcoin.createNewBlock(998764, 'AKMC875ZYS1TD9', 'WPLS214R7T6SJ292');
 
-console.log(bitcoin);
+// console.log(bitcoin);
+
+
+const previousBlockHash = '87765DA6CCF0668238C1D27C35692E11';
+const currentBlockData = [
+  {
+    amount: 10,
+    sender: 'B4CEE9C0E5CD571',
+    recipient: '3A3F6E462D48E9',
+  }
+];
+const nonce = 100;
+
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
