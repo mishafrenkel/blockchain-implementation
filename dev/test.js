4108b39e2ce04b4c081be8d0c10e46eaafb59e98
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-08 20:44:06 -0700

    implement hashBlock function

--- dev/test.js
+++ dev/test.js
@@ -2,12 +2,28 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
+// Testing creating Blocks
 // bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
 // bitcoin.createNewBlock(2389, 'OIUOEREDHKHKD', '78s97d4x6dsf');
 // bitcoin.createNewBlock(2389, 'OKJOERETHKHKD', '78s97d4x6dsf');
 // bitcoin.createNewBlock(2389, 'OIUOERDEGWKHKD', '78s97d4x6dsf');
 
+// Testing creatingNewBlock, then adding pending transaction, then creating newblock which adds the transaction
+// bitcoin.createNewBlock(789457, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56');
+// bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+// bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WPLS214R7T6SJ3G2');
+// console.log(bitcoin.chain[1])
+
+
 bitcoin.createNewBlock(789457, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56');
+
 bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+
 bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WPLS214R7T6SJ3G2');
-console.log(bitcoin.chain[1])
+
+bitcoin.createNewTransaction(50, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+bitcoin.createNewTransaction(200, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+bitcoin.createNewTransaction(300, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+bitcoin.createNewBlock(998764, 'AKMC875ZYS1TD9', 'WPLS214R7T6SJ292');
+
+console.log(bitcoin);
