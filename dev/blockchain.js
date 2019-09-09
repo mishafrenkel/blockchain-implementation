68192dc27b1e09286f22989fbe661659a80f1450
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-08 20:10:10 -0700

    code getLastBlock function

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,33 +1,6 @@
-// function Blockchain() {
-//   this.chain = [];
-//   this.newTransactions = [];
-// }
-
-// Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
-//   const newBlock = {
-//     index: this.chain.length + 1,
-//     timestamp: Date.now(),
-//     transactions: this.newTransactions,
-//     nonce: nonce,
-//     hash: hash,
-//     previousBlockHash: previousBlockHash
-//   };
-
-//   this.newTransaction = [];
-//   this.chain.push(newBlock);
-
-//   return newBlock;
-// };
-
-// Blockchain.prototype.getLastBlock = function () {
-
-// };
-
-class Blockchain {
-  constructor() {
-    this.chain = [];
-    this.newTransactions = [];
-  }
+function Blockchain() {
+  this.chain = [];
+  this.newTransactions = [];
 }
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
@@ -39,9 +12,20 @@ Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) 
     hash: hash,
     previousBlockHash: previousBlockHash
   };
+
   this.newTransaction = [];
   this.chain.push(newBlock);
+
   return newBlock;
 };
+
+Blockchain.prototype.getLastBlock = function () {
+  return this.chain[this.chain.length - 1];
+};
+
+Blockchain.prototype.createNewTransaction = function () {
+
+}
+
 
 module.exports = Blockchain;
